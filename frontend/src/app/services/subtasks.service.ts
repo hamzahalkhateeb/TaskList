8e9925a0b7d2fda96a1586689b1867e402f90759
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2025-08-26 16:51:42 +0930

    added task and subtask services

---
+++ frontend/src/app/services/subtasks.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, catchError, of } from 'rxjs';
+import { Task } from '../models/task.model';
+import { Subtask } from '../models/subtask.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SubtasksService {
+  private backendUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  //add subtask to a task by task id
+  addSubtask(taskId: number, subtask: Partial<Subtask>): Observable<Subtask> {
+    return this.http
+      .post<Subtask>(`${this.backendUrl}/tasks/${taskId}/subtasks`, subtask)
+      .pipe(
+        catchError((error) => {
+          console.error('Error adding subtask', error);
+          return of();
+        })
+      );
+  }
+
+  //complete subtask by subtask id and task id
+  completeSubtask(taskId: number, subtaskId: number): Observable<Subtask> {
+    return this.http
+      .patch<Subtask>(
+        `${this.backendUrl}/tasks/${taskId}/subtasks/${subtaskId}/complete`,
+        {}
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Error completing subtask', error);
+          return of();
+        })
+      );
+  }
+
+  //delete subtask by subtask id and task id
+  deleteSubtask(taskId: number, subtaskId: number): Observable<void> {
+    return this.http
+      .delete<void>(`${this.backendUrl}/tasks/${taskId}/subtasks/${subtaskId}`)
+      .pipe(
+        catchError((error) => {
+          console.error('Error deleting subtask', error);
+          return of();
+        })
+      );
+  }
+}
---
+++ frontend/src/app/services/tasks.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, catchError, of } from 'rxjs';
+import { Task } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  //needed variables
+  private apiUrl = 'http://localhost:3000'; //Backend API URL
+
+  //constructor
+  constructor(private http: HttpClient) {}
+
+  //get all tasks, take a time frame as optional parameter
+  getAllTasks(fromDate?: string, toDate?: string): Observable<Task[]> {
+    //get current date
+    const now = new Date();
+    
+
+    //if no time frame is provided, set default to one week before and one week after current date AND convert to ISO string
+    const from =
+      fromDate ||
+      new Date(new Date(now).setDate(now.getDate() - 7)).toISOString();
+
+    const to =
+      toDate ||
+      new Date(new Date(now).setDate(now.getDate() + 7)).toISOString();
+
+    // Fetch tasks within the given or default time frame
+    return this.http
+      .get<Task[]>(`${this.apiUrl}/tasks?from=${from}&to=${to}`)
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching tasks', error);
+          return of([]);
+        })
+      );
+  }
+
+  //delete a task by task id
+  deleteTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/tasks/${taskId}`).pipe(
+      catchError((error) => {
+        console.error('Error deleting task', error);
+        return of();
+      })
+    );
+  }
+
+
+  //create a new task
+  createTask(task: Partial<Task>): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/tasks`, task).pipe(
+      catchError((error) => {
+        console.error('Error creating task', error);
+        return of();
+      })
+    );
+  }
+
+  //update a task by task id
+  updateTask(taskId: number, updatedTask: Partial<Task>): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/tasks/${taskId}`, updatedTask).pipe(
+      catchError((error) => {
+        console.error('Error updating task', error);
+        return of();
+      }));
+  }
+
+
+  //complete a task by id
+  completeTask(taskId: number): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/tasks/${taskId}/complete`, {}).pipe(
+      catchError((error) => {
+        console.error('Error completing task', error);
+        return of();
+      })
+    );
+  }
+  
+  
+  
+
+
+}
