f4e820426985baf6c09ca364d1b1094da241cc6f
hamzahalkhateeb <hamzahalkhateeb42@gmail.com>
2025-08-27 17:21:09 +0930

    declared task and subtask model

---
+++ backend/Models/Subtask.cs
@@ -0,0 +1,11 @@
+namespace Backend.Models
+{
+    public class Subtask
+    {
+        
+            public int Id { get; set; }
+            public string Title { get; set; } = string.Empty;
+            public bool IsCompleted { get; set; } = false;
+        
+    }
+}
---
+++ backend/Models/Task.cs
@@ -0,0 +1,15 @@
+using Backend.Models;
+namespace backend.Models
+
+{
+    public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; }
+    public string Priority { get; set; } = "low";
+    public bool isCompleted { get; set; } = false;
+    public List<Subtask> Subtasks { get; set; } = new List<Subtask>();
+}
+}
